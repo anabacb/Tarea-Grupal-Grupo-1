7f5bc5c9437b01c23b8f1678ba411017cbdb323f
anabacb <abacb@miumg.edu.gt>
2024-03-30 21:52:11 -0600

    primer_commit

---
+++ Instrucciones tarea Grupal.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// Formar Grupos de 5 Personas máximo .
+Crear un repositorio en GitHub para colaborar como grupo.
+Distribuir de forma equitativa la elaboración de los programas.
+Al finalizar todos los integrantes del grupo deben de tener  en sus equipos el código fuente  actualizado  y evidenciar  en el Insights la colaboración de cada integrante del grupo
+Debe de crear un menú principal para seleccionar cada uno de los programas (en este archivo todos deben de colaborar agregando cada quien lo que corresponde).
+Aplicar buenas prácticas de programación.
+Crear los siguientes Programas en C++
+
+Suma, Resta, Multiplicación y División de dos números
+Determinar si un número es par o impar
+Convertir de kilómetros a millas, metros a pulgadas, de libras a kilos y viceversa
+Determinar si una palabra o un número es palíndromo.
+Conversión de números arábigos a romanos (mínimo 1,000)
+Conversión de números enteros a letras
+Conversión de números enteros con decimal a letras
+Una tabla de multiplicar
+Todas las tablas de multiplicar del 1 al 10
+Crear de forma gráfica la multiplicación manual
+                          45
+                       X 32
+                         90  
+                     135  
+                   1440
+
+Conversión de números decimales a binario
+Conversión de números decimales a hexadecimales
+Crear Figuras Geométricas Básicas.
+Mover un punto en toda la pantalla
+Simulación de un Cajero (Autómata)
+Calcular la hipotenusa
+Libre (pueden realizar el programa que ustedes quieran)
+Libre (pueden realizar el programa que ustedes quieran)
+Libre (pueden realizar el programa que ustedes quieran)
+Libre (pueden realizar el programa que ustedes quieran)
+ Menú Principal. (este archivo debe de colaborar todos y todos deben de tener aportes a este archivo)\\
